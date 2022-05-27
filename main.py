dab97ee53d43e9f6dc1d733ecc67223a64a7fcad
Matthias Petursson <matthias01123@gmail.com>
2022-05-27 21:54:52 +0000

    Premature coding start..

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame as pg
+
+if not pg.font:
+    print("Warning, fonts disabled")
+if not pg.mixer:
+    print("Warning, sound disabled")
+
+def main():
+    # ...
+
+
+if __name__ == "__main__":
+    main()
