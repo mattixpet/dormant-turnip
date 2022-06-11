7d634a541a286da3e87592cb5f39a9626f20d5c4
Matthias Petursson <matthias01123@gmail.com>
2022-06-11 17:03:28 +0000

    Add images for soldier and swagavulin and a bit of code reorganization.

---
+++ game/game.py
@@ -0,0 +1,16 @@
+import pygame as pg
+
+if not pg.font:
+    print("Warning, fonts disabled")
+if not pg.mixer:
+    print("Warning, sound disabled")
+
+def start_game():
+    '''
+    Sets everything up and then starts the game.
+    '''
+    print("hello")
+
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-import pygame as pg
-
-if not pg.font:
-    print("Warning, fonts disabled")
-if not pg.mixer:
-    print("Warning, sound disabled")
+from game.game import start_game
 
 def main():
-    # ...
-
+    start_game()
 
 if __name__ == "__main__":
     main()
